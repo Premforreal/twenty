7aebde60ce73b228c3f5120691bad78af29776a3
neo773 <neo773@protonmail.com>
2025-10-28 21:22:07 +0530

    wip

--- packages/twenty-server/src/modules/messaging/message-channel-manager/query-hooks/message-channel-query-hook.module.ts
+++ packages/twenty-server/src/modules/messaging/message-channel-manager/query-hooks/message-channel-query-hook.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { MessageChannelUpdateOnePreQueryHook } from 'src/modules/messaging/message-channel-manager/query-hooks/message-channel-update-one.pre-query.hook';
-import { MessagingProcessGroupEmailActionsService } from 'src/modules/messaging/message-import-manager/services/messaging-process-group-email-actions.service';
+import { MessagingImportManagerModule } from 'src/modules/messaging/message-import-manager/messaging-import-manager.module';
 
 @Module({
-  providers: [
-    MessageChannelUpdateOnePreQueryHook,
-    MessagingProcessGroupEmailActionsService,
-  ],
+  imports: [MessagingImportManagerModule],
+  providers: [MessageChannelUpdateOnePreQueryHook],
   exports: [MessageChannelUpdateOnePreQueryHook],
 })
 export class MessageChannelQueryHookModule {}
